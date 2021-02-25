765b0323a1c15c25a70fbe9b73eb280cbfaf05df
GreeSol <greesol255@gmail.com>
2021-02-25 23:22:28 +0100

    Zadania 1-4

---
+++ src/first_week/basics/lekcja_basics.java
@@ -0,0 +1,30 @@
+package first_week.basics;
+
+public class lekcja_basics {
+    public static void main(String[] args) {
+        //Pierwsze zadanie
+        int myIntA = 5;
+        double myDoubleA = 1.67;
+        char myCharB = 'T';
+        String myStringA = "Michał";
+        System.out.println(myIntA);
+        System.out.println(myDoubleA);
+        System.out.println(myCharB);
+        System.out.println(myStringA);
+        //Drugie zadanie
+                int myIntB = 6;
+                int myIntC = 3;
+        System.out.println("Wynik:");
+        System.out.println(myIntB * myIntB + myIntC * myIntC);
+        //Trzecie zadanie
+        double sqrt = Math.sqrt(3);
+        int bok=7;
+        System.out.println("wynik:" );
+        System.out.println(bok * bok * sqrt /4);
+        //Czwarte zadanie
+        String imString= "Michał";
+        String naString= "Mazurowski";
+        System.out.println(imString + naString);
+
+    }
+}
