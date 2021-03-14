d95eb85328a0eeb07726214961ef8a3a4b857d39
GreeSol <greesol255@gmail.com>
2021-03-14 18:56:09 +0100

    Update dziedziczenie.java

--- src/third_week/dziedziczenie.java
+++ src/third_week/dziedziczenie.java
@@ -5,31 +5,26 @@ public class dziedziczenie {
     public static class Vehicle {
         protected final int tankCapacity = 60;
         protected int fuelLevel = 40;
-
         public void fillTank() {
             int toFill = tankCapacity - fuelLevel;
             fillTank(toFill);
         }
-
         public void fillTank(int toFill){
             if (toFill + fuelLevel > tankCapacity){
-                System.out.println("Nie moge wypelnic baku " + toFill + " litrami.");
+                System.out.println("Nie moge wlać do baku " + toFill + " litrów.");
             }
             else{
-                fuelLevel += toFill; //uzupełnienie paliwa ilością "toFill"
-                System.out.println("Wypelniono bak " + toFill + " litrami.");
+                fuelLevel += toFill;
+                System.out.println("Wlano " + toFill + " litrów.");
             }
         }
     }
-
     public static void main(String[] args) {
         Vehicle vehicle = new Vehicle();
-        System.out.println("Pojemność baku: " + vehicle.tankCapacity);
-        System.out.println("Poziom paliwa: " + vehicle.fuelLevel);
-        vehicle.fillTank(); //uzupełnienie paliwa do maksa
-        System.out.println("Poziom paliwa: " + vehicle.fuelLevel);
-        vehicle.fillTank(10); //nieudana próba dolania paliwa
-
+        System.out.println("Maks w baku: " + vehicle.tankCapacity);
+        System.out.println("Ile jest w paliwa: " + vehicle.fuelLevel);
+        vehicle.fillTank();
+        System.out.println("Ile jest w paliwa: " + vehicle.fuelLevel);
+        vehicle.fillTank(10);
     }
-
 }
